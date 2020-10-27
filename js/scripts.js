6a0995d65a005d1bebea7ce5cd9f50d6d70e97eb
Amanda Otero <mandystar018@gmail.com>
2020-10-26 22:38:31 -0700

    add forEach loop and made variable an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  $('#form').submit(function(event) {
+    event.preventDefault();
 
+    const userInput = $('#sentence').val();
+    const array = userInput.split();
+    
+    array.forEach(function(element) {
+      let newArray = [];
+      newArray.push(element);
+    });
+  });
 });
